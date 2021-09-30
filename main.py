311e4e63841bbb8507ccc0512ffbdcbdb0c108ce
Quang Bá <ba.24.04.2002@gmail.com>
2021-09-30 15:07:06 +0700

    thay doi

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests as requests
 EXPO_SERVER_URL = 'https://exp.host/--/api/v2/push/send'
 token1 = 'ExponentPushToken[sq62WdJukpCP5A0_A5iGZf]'
 token2 = 'ExponentPushToken[6EsjURJd2Nl7LsyY9tBJVy]'
-with open("data3.json", "r") as read_file:
+with open("data4.json", "r") as read_file:
     data = json.load(read_file)
 for x in range(len(data)):
     requests.post(EXPO_SERVER_URL, {"to": token1, "title": data[x]['title'],
@@ -14,5 +14,5 @@ for x in range(len(data)):
     requests.post(EXPO_SERVER_URL, {"to": token2, "title": data[x]['title'],
                                     "body": data[x]['body']})
     print((data[x]['title']))
-    if x != len(data)-2:
+    if x in range(1, len(data)-2):
         time.sleep(600)
